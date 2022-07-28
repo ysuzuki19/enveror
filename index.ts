7f594a46a7e654790c75a8f142163e923fb53247
ysuzuki19 <ysuzuki.cpp@gmail.com>
2022-07-29 01:39:30 +0900

    refact

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
-import { Enveror } from './lib/loader';
+import path from 'path';
+import { Enveror } from './lib/enveror';
 
 export function print_root() {
   // const none_env_path = process.cwd() + '/.env';
-  const local_env_path = process.cwd() + '/.enveror.local';
-  const enveror = new Enveror([local_env_path]);
-  console.log(enveror.to_string());
+  const local_env_path = path.join(process.cwd() + '/.enveror.local');
+  const enveror = new Enveror({ routes: [local_env_path] });
+  console.dir(enveror.to_string());
 }
 print_root();
